2db74668c4572c13b7c3f2e11c71ab2c0a2ff972
Trần Anh Đức <ducanhtran28112x@gmail.com>
2024-09-19 16:37:11 +0700

    add config JpaAuditing

--- src/main/java/com/datn/beestyle/BeBeestyleSpringBootApplication.java
+++ src/main/java/com/datn/beestyle/BeBeestyleSpringBootApplication.java
@@ -2,8 +2,10 @@ package com.datn.beestyle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareRef")
 public class BeBeestyleSpringBootApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/datn/beestyle/entity/Auditable.java
+++ src/main/java/com/datn/beestyle/entity/Auditable.java
@@ -12,11 +12,11 @@ public abstract class Auditable<T extends Serializable> extends BaseEntity<T> {
 
     // user tạo mới record
     @CreatedBy
-    @Column(name = "created_by")
+    @Column(name = "created_by", updatable = false)
     private T createdBy;
 
     // user update cuối cùng
     @LastModifiedBy
-    @Column(name = "updated_by")
+    @Column(name = "updated_by", insertable = false)
     private T updateBy;
 }
