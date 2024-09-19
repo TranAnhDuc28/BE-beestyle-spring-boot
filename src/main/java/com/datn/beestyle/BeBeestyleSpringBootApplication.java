a872421eb062359868255dc0a7098583204dccfa
Trần Anh Đức <ducanhtran28112x@gmail.com>
2024-09-19 10:58:45 +0700

    first commit: create project

---
+++ src/main/java/com/datn/beestyle/BeBeestyleSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.datn.beestyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeBeestyleSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeBeestyleSpringBootApplication.class, args);
+    }
+
+}
