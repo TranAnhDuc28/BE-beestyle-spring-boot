f9f18c87706c9295ef762f3c852f860899682c5d
Trần Anh Đức <ducanhtran28112x@gmail.com>
2024-09-19 18:43:46 +0700

    update exception handler

--- src/main/java/com/datn/beestyle/exception/GlobalExceptionHandler.java
+++ src/main/java/com/datn/beestyle/exception/GlobalExceptionHandler.java
@@ -20,68 +20,48 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({ResourceNotFoundException.class})
-    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(Exception e, WebRequest request) {
-        String path = request.getDescription(false).replace("uri=", "");
-
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setTimestamp(LocalDateTime.now());
-        errorResponse.setCode(HttpStatus.NOT_FOUND.value());
-        errorResponse.setPath(path);
-        errorResponse.setError(HttpStatus.NOT_FOUND.getReasonPhrase());
-        errorResponse.setMessage(e.getMessage());
-
-        log.error("Path: {} - Msg error: {}",path, e.getMessage());
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException e, WebRequest request) {
+        ErrorResponse errorResponse = this.createErrorResponse(HttpStatus.NOT_FOUND, e.getMessage(), request);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
     @ExceptionHandler({MethodArgumentNotValidException.class})
     public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e, WebRequest request) {
-        String path = request.getDescription(false).replace("uri=", "");
-
         Map<String, String> errors = new HashMap<>();
         BindingResult bindingResult = e.getBindingResult();
         for (FieldError fieldError: bindingResult.getFieldErrors()) {
             errors.put(fieldError.getField(), fieldError.getDefaultMessage());
         }
 
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setTimestamp(LocalDateTime.now());
-        errorResponse.setCode(HttpStatus.BAD_REQUEST.value());
-        errorResponse.setPath(path);
+        ErrorResponse errorResponse = this.createErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage(), request);
         errorResponse.setError("Invalid Payload");
-        errorResponse.setMessage(errors);
-
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler({InvalidDataException.class, EmptyResultDataAccessException.class})
     public ResponseEntity<ErrorResponse> handleInvalidDataException(Exception e, WebRequest request) {
-        String path = request.getDescription(false).replace("uri=", "");
-
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setTimestamp(LocalDateTime.now());
-        errorResponse.setCode(HttpStatus.CONFLICT.value());
-        errorResponse.setPath(path);
-        errorResponse.setError(HttpStatus.CONFLICT.getReasonPhrase());
-        errorResponse.setMessage(e.getMessage());
-
-        log.error("Path: {} - Msg error: {}",path, e.getMessage());
+        ErrorResponse errorResponse = this.createErrorResponse(HttpStatus.CONFLICT, e.getMessage(), request);
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleInternalServerErrorException(Exception e, WebRequest request) {
+        ErrorResponse errorResponse = this.createErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), request);
+        log.info("Class exception: {}", e.getClass());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+
+    private ErrorResponse createErrorResponse(HttpStatus httpStatus, String message, WebRequest request) {
         String path = request.getDescription(false).replace("uri=", "");
 
         ErrorResponse errorResponse = new ErrorResponse();
         errorResponse.setTimestamp(LocalDateTime.now());
-        errorResponse.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorResponse.setCode(httpStatus.value());
         errorResponse.setPath(path);
-        errorResponse.setError(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
-        errorResponse.setMessage(e.getMessage());
+        errorResponse.setError(httpStatus.getReasonPhrase());
+        errorResponse.setMessage(message);
 
-        log.info("Class exception: {}", e.getClass());
-        log.error("Path: {} - Msg error: {}",path, e.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+        log.error("Path: {} - Msg error: {}",path, message);
+        return errorResponse;
     }
 }
