5f5d43c5328fac0bf39ee1f2e7c4e68b41c3f725
Trần Anh Đức <ducanhtran28112x@gmail.com>
2024-09-21 12:04:00 +0700

    add anotation @FieldDefaults(level = AccessLevel.PRIVATE) cho các entity

--- src/main/java/com/datn/beestyle/entity/Auditable.java
+++ src/main/java/com/datn/beestyle/entity/Auditable.java
@@ -2,21 +2,24 @@ package com.datn.beestyle.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 
 import java.io.Serializable;
 
 @MappedSuperclass
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public abstract class Auditable<T extends Serializable> extends BaseEntity<T> {
 
     // user tạo mới record
     @CreatedBy
     @Column(name = "created_by", updatable = false)
-    private T createdBy;
+    T createdBy;
 
     // user update cuối cùng
     @LastModifiedBy
     @Column(name = "updated_by", insertable = false)
-    private T updateBy;
+    T updateBy;
 }
--- src/main/java/com/datn/beestyle/entity/BaseEntity.java
+++ src/main/java/com/datn/beestyle/entity/BaseEntity.java
@@ -1,6 +1,8 @@
 package com.datn.beestyle.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -8,22 +10,23 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public abstract class BaseEntity<T extends Serializable> implements Serializable{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private T id;
+    T id;
 
     // lưu thời gian record được tạo ra
     @Column(name = "created_at")
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
-    private LocalDateTime createdAt;
+    LocalDateTime createdAt;
 
     // lưu thời gian record được update lần cuối
     @Column(name = "updated_at")
     @UpdateTimestamp
     @Temporal(TemporalType.TIMESTAMP)
-    private LocalDateTime updatedAt;
+    LocalDateTime updatedAt;
 }
