d355580d65ca32013f466bd450e64b323c4d4a73
Trần Anh Đức <ducanhtran28112x@gmail.com>
2024-09-21 12:34:54 +0700

    update getter/setter class  BaseEntity and Auditable

--- src/main/java/com/datn/beestyle/entity/BaseEntity.java
+++ src/main/java/com/datn/beestyle/entity/BaseEntity.java
@@ -2,6 +2,8 @@ package com.datn.beestyle.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -9,6 +11,8 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @MappedSuperclass
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public abstract class BaseEntity<T extends Serializable> implements Serializable{
