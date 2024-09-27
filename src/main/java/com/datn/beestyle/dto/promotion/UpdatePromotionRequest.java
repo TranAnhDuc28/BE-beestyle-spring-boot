993c4cdd7d1859f440fe53a61cb56d7c7f02076b
TranAnhHoang <hoangtaph35687@fpt.edu.vn>
2024-09-27 20:33:32 +0700

    update promotion

--- src/main/java/com/datn/beestyle/dto/promotion/UpdatePromotionRequest.java
+++ src/main/java/com/datn/beestyle/dto/promotion/UpdatePromotionRequest.java
@@ -1,32 +1,43 @@
 package com.datn.beestyle.dto.promotion;
 
 import com.datn.beestyle.enums.DiscountType;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UpdatePromotionRequest {
+
     @NotNull(message = "ID cannot be null")
     Integer id;
 
+    @NotBlank(message = "Promotion name cannot be blank")
     String promotionName;
 
+    @NotNull(message = "Discount type cannot be null")
     DiscountType discountType;
 
+    @NotNull(message = "Discount value cannot be null")
+    @Positive(message = "Discount value must be positive")
     Integer discountValue;
 
+    @NotNull(message = "Start date cannot be null")
     Timestamp startDate;
 
+    @NotNull(message = "End date cannot be null")
     Timestamp endDate;
-    Integer updatedBy;
-    String description;
 
+    @NotNull(message = "Updated by cannot be null")
+    Integer updatedBy;
+
+    String description;
+    Boolean deleted;
 }
