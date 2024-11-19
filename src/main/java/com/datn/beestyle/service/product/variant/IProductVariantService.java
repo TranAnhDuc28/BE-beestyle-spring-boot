7f052ade3991f0610851c761d8a090837a79ba45
quangvdph31622 <quangvdph31622@fpt.edu.vn>
2024-11-19 20:40:58 +0700

    update new promotion

--- src/main/java/com/datn/beestyle/service/product/variant/IProductVariantService.java
+++ src/main/java/com/datn/beestyle/service/product/variant/IProductVariantService.java
@@ -26,7 +26,4 @@ public interface IProductVariantService
     Optional<Object[]> getAllProductsWithDetails(List<Long> productIds);
 
     void updateProductVariantCreate(Integer promotionId, List<Integer> ids);
-
-    void updateProductVariantUpdate(Integer promotionId, List<Integer> ids);
-
 }
