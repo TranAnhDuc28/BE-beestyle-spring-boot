0ed9d5ebfb4426a8b3f2ac25bcd62ff892650ac4
Trần Anh Đức <ducanhtran28112x@gmail.com>
2024-12-28 22:31:06 +0700

    update code order service

--- src/main/java/com/datn/beestyle/enums/PaymentMethod.java
+++ src/main/java/com/datn/beestyle/enums/PaymentMethod.java
@@ -5,11 +5,20 @@ import org.springframework.lang.Nullable;
 
 @Getter
 public enum PaymentMethod {
-    CASH_ON_DELIVERY(0),
-    BANK_TRANSFER(1),
-    COD_AND_BANK_TRANSFER(2);
+    // thanh toán tiền mặt tại quầy (kênh bán trực tiếp)
+    // thanh toán tiền mặt khi nhận hàng (kênh bán trực tuyến)
+    CASH(0), // tiền mặt
+
+    // thanh toán qua chuyển khoản ngân hàng (kênh bán trực tiếp)
+    // thanh toán trước toàn bộ qua chuyển khoản trước khi giao hàng. (kênh bán trực tuyến)
+    BANK_TRANSFER(1), // chuyển khoản
+
+    // thanh toán 1 phần tền mặt vằ 1 phần qua chuyển khoản ngân hàng (kênh bán trực tiếp)
+    // thanh toán một phần qua chuyển khoản trước, phần còn lại bằng tiền mặt khi nhận hàng. (kênh bán trực tuyến)
+    CASH_AND_BANK_TRANSFER(2); // thanh toán và chuyển khoản
 
     private final int value;
+
     PaymentMethod(int value) {
         this.value = value;
     }
