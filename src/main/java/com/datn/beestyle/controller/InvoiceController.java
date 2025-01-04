e418cfc116ceebc7e00746cfa725ef0b45f969d9
loc131024 <locnhph38787@fpt.edu.vn>
2025-01-04 21:53:33 +0700

    update invoice

--- src/main/java/com/datn/beestyle/controller/InvoiceController.java
+++ src/main/java/com/datn/beestyle/controller/InvoiceController.java
@@ -75,9 +75,11 @@ import com.datn.beestyle.util.InvoicePDFExporter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 
 import java.io.ByteArrayOutputStream;
 
@@ -88,6 +90,7 @@ public class InvoiceController {
     private final OrderRepository orderRepository;
     private final OrderItemRepository orderItemRepository;
     private final InvoicePDFExporter invoicePDFExporter;
+    private static final Logger logger = LoggerFactory.getLogger(InvoiceController.class);
 
     /**
      * API để xuất hóa đơn PDF từ thông tin đơn hàng
@@ -119,4 +122,36 @@ public class InvoiceController {
             return ResponseEntity.status(500).body(null);
         }
     }
+    @GetMapping("/preview/{orderId}")
+    public ResponseEntity<byte[]> previewInvoice(@PathVariable Long orderId) {
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            invoicePDFExporter.exportInvoice(orderId, outputStream);
+            byte[] pdfBytes = outputStream.toByteArray();
+
+            if (pdfBytes.length == 0) {
+                logger.warn("Preview PDF content is empty.");
+                return ResponseEntity.noContent().build();
+            }
+
+            // Tên file với ID đơn hàng và ngày tháng
+            String fileName = "Invoice_" + orderId  + ".pdf";
+
+            logger.info("Invoice preview generated successfully for order: {}", orderId);
+
+            // Thiết lập các header cho response
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + fileName + "\"");
+            headers.add(HttpHeaders.CONTENT_TYPE, "application/pdf");
+            headers.add("Access-Control-Allow-Headers", "Content-Disposition");
+//            headers.add("Access-Control-Allow-Origin", "*");  // Đảm bảo CORS nếu frontend và backend ở domain khác
+
+            return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
+
+        } catch (Exception e) {
+            logger.error("Error generating invoice preview: {}", e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
 }
