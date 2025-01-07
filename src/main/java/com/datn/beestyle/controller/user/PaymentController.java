65063faf2da3e7d0c9ef934af0718717d660e1f1
anhvdph33906 <anhvdph33906@fpt.edu.vn>
2025-01-07 23:40:52 +0700

    jsjsksjdhgvgydus

---
+++ src/main/java/com/datn/beestyle/controller/user/PaymentController.java
@@ -0,0 +1,28 @@
+package com.datn.beestyle.controller.user;
+
+import com.datn.beestyle.dto.ApiResponse;
+import com.datn.beestyle.dto.order.CreateOrderOnlineRequest;
+import com.datn.beestyle.service.order.IOrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+public class PaymentController {
+
+    private final IOrderService orderService;
+
+    @PostMapping("/checkout")
+    public ApiResponse<?> checkout(@Valid @RequestBody CreateOrderOnlineRequest request) {
+        return new ApiResponse<>(HttpStatus.OK.value(), "Order online created successfully.",
+                orderService.createOrderOnline(request));
+    }
+}
--- src/main/java/com/datn/beestyle/dto/order/CreateOrderOnlineRequest.java
+++ src/main/java/com/datn/beestyle/dto/order/CreateOrderOnlineRequest.java
@@ -10,11 +10,13 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 
 import java.math.BigDecimal;
 @Getter
 @Builder
+@ToString
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CreateOrderOnlineRequest {
     Long customerId;
